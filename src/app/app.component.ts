8b1c00a7753cfaa69c2625b2a3351144c277fac7
Salsabeal-Azzam <salsabealazzam@gmail.com>
2022-11-05 17:06:00 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'finalproject';
+
+   constructor(private _AuthenticationService:AuthenticationService)
+  {
+
+
+
+  }
+
+}
